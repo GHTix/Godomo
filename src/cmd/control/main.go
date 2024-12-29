ef39bac5a478ee4297bf96440aada0ac41a6d9d3
GHTix <twix.email@gmail.com>
2024-12-29 18:32:57 +0100

    feat: control boilerplate

--- src/cmd/control/main.go
+++ src/cmd/control/main.go
@@ -1,32 +1,69 @@
 package main
 
 import (
-	"github.com/ghtix/gomodo/cmd/services/aqara/docs"
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	controlConfig "github.com/ghtix/gomodo/cmd/control/config"
+	"github.com/ghtix/gomodo/internal/config"
 	"github.com/gin-gonic/gin"
-	swaggerfiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
-	// config, err := config.New("../private/config.yml")
-	// if err != nil {
-	// 	slog.Error("main", "error loading config", err.Error())
-	// }
+	slog.Info("CONTROL")
+
+	configFile := os.Getenv("GODOMO_CONTROL_CONFIG_PATH")
+	config, err := config.New[controlConfig.ControlConfig](configFile)
+	if err != nil {
+		slog.Error("main", "error loading config", err.Error(), "file", configFile)
+		return
+	} else {
+		slog.Info("main", "Config OK from", configFile)
+	}
+
+	for _, svc := range config.ControlledServices {
+		slog.Info("Service", "Name", svc.Name, "Port", svc.Port)
+	}
 
 	router := gin.Default()
 
-	// h := handler.New()
+	SetupRouter(router)
 
-	var v1 = router.Group("/api/v1")
-	v1.Use()
-	{
+	slog.Info(config.Service.ListeningAddress)
+
+	server := &http.Server{
+		Addr:    config.Service.ListeningAddress,
+		Handler: router.Handler(),
 	}
 
-	// Swagger
-	docs.SwaggerInfo.BasePath = "/"
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
-	router.Run(":8880")
+	go func() {
+		// service connections
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("Listen", "Err", err.Error())
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+
+	// Cleanup
+	slog.Info("Clean up..")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Error while shutting down Server. Initiating force shutdown...", "err", err)
+	} else {
+		slog.Info("Server exiting")
+	}
 
 }
